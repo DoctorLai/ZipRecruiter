2e76e5276ee53e1b64a57a6e9e8a505fb2601af5
@justyy <oh@steakovercooked.com>
2018-01-26 17:03:20 +0000

    Create job_search_Test.php

---
+++ tests/job_search_Test.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../class.utopian.php';
+
+use PHPUnit\Framework\TestCase;
+
+class JobSearchTests extends TestCase {
+    // TODO
+}
