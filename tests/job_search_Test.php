858776ad574d8f448f4e5b0e72def68506b216b2
DoctorLai <dr.zhihua.lai@gmail.com>
2021-09-18 09:56:42 +0100

    Delete tests directory

--- tests/job_search_Test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../class.utopian.php';
-
-use PHPUnit\Framework\TestCase;
-
-class JobSearchTests extends TestCase {
-    // TODO
-}
